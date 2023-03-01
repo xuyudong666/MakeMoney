41233a183b27d7bc2f764e5fe140941d0cea2d35
xuyd22082203 <xuyd22082203@q1.com>
2023-03-01 14:27:53 +0800

    修改domain.core命名

--- MakeMoney.Domain/Students/Student.cs
+++ MakeMoney.Domain/Students/Student.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace MakeMoney.Domain.Students
-{
-    public class Student
-    {
+namespace MakeMoney.Domain.Students {
+    public class Student {
 
-        private Student()
-        {
+        private Student() {
 
         }
 
-        internal Student(string name)
-        {
+        internal Student(string name) {
             Name = name;
         }
 
         public int Id { get; private set; }
         public string Name { get; private set; }
-
         public void SetName(string name) {
+            //if（长度大于20 必须是中文）
             Name = name;
         }
+
+        //payOrder() {
+
+        //}
     }
 }
