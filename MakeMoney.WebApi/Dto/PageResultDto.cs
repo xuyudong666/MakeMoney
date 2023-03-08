e9540f43f9f85dbbede9f50cdc45248d6f938c9e
xuyd22082203 <xuyd22082203@q1.com>
2023-03-08 15:08:10 +0800

    增加Dto

---
+++ MakeMoney.WebApi/Dto/PageResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MakeMoney.WebApi.Dto {
+    public class PageResultDto {
+        public const int MaxPageSize = 50;
+        private int _pageSize = 10;
+        public int PageIndex { get; set; } = 1;
+        public int PageSize {
+            get { return _pageSize; }
+            set { _pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+    }
+}
